735de6621396832c4631196af05385d3663f3149
Benny Thadikaran <tbenny05@yahoo.com>
2025-06-14 20:41:38 +0530

    merge install_base into pre_chroot.sh

--- install_script/pre_chroot.sh
+++ install_script/pre_chroot.sh
@@ -12,9 +12,26 @@ fi
 echo "Enabling system time synchronization"
 timedatectl set-ntp true
 
+packages=(
+  base
+  base-devel
+  dosfstools
+  linux-lts
+  linux-zen
+  linux-lts-headers
+  linux-zen-headers
+  linux-firmware
+  grub
+  efibootmgr
+  amd-ucode
+)
+
 # 4. Set up the mirrorlist using reflector
 echo "Updating mirrorlist"
 reflector -c IN --age 12 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
+
+echo "Installing base system and essential packages"
+pacstrap /mnt "${PACKAGES[@]}" 2>&1 | tee -a pacstrap.log
 
 # 2. Generate fstab with UUIDs
 echo "Generating fstab"
