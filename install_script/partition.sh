227009b70192ec893c790f6e66761768bfc8c199
Benny Thadikaran <tbenny05@yahoo.com>
2025-06-16 10:59:12 +0530

    - Removed package install commands. Will be run manually. - Consolidated bash script into 3 files.

--- install_script/partition.sh
+++ install_script/partition.sh
@@ -9,7 +9,7 @@ set -e
 #
 # vda1  512MB EFI FAT32
 # vda2  100GB EXT4 root
-# vda3  remaining EXT4 /home encrypted with luks
+# vda3  /home remaining space
 
 # Ensure the user provides a device name as an argument
 if [ -z "$1" ]; then
@@ -29,11 +29,11 @@ fi
 echo "Partitioning $DEVICE..."
 
 # Create GPT partition table
-parted --script $DEVICE mklabel gpt
+parted --align optimal --script $DEVICE mklabel gpt
 
 # Create EFI partition (512 MB)
 parted --script $DEVICE mkpart primary fat32 1MiB 513MiB
-parted --script $DEVICE set 1 boot on # Mark partition 1 as bootable (EFI)
+parted --script $DEVICE set 1 esp on
 
 # Create root partition (100 GB)
 parted --script $DEVICE mkpart primary ext4 513MiB 100.5GiB
@@ -44,48 +44,22 @@ parted --script $DEVICE mkpart primary ext4 100.5GiB 100%
 # Print the partition table
 parted $DEVICE print
 
-# Format EFI partition
+# Format partitions
 mkfs.fat -F32 "${DEVICE}1"
-
-# Format root partition
-mkfs.ext4 "${DEVICE}2"
-
-# Encrypt the home partition
-echo "Encrypting /home partition..."
-cryptsetup luksFormat "${DEVICE}3"
-
-echo "Disk encryption complete. Reenter passphrase to decrypt and mount home partition"
-cryptsetup open "${DEVICE}3" crypt_home
-
-# Format encrypted /home partition
-mkfs.ext4 /dev/mapper/crypt_home
+mkfs.ext4 -F "${DEVICE}2"
+mkfs.ext4 -F "${DEVICE}3"
 
 # Mount the partitions
 echo "Mounting partitions..."
 mount "${DEVICE}2" /mnt
-mkdir /mnt/boot
+mkdir /mnt/{boot,home}
 mount "${DEVICE}1" /mnt/boot
-mkdir /mnt/home
-mount /dev/mapper/crypt_home /mnt/home
-
-# Get the UUID using blkid
-UUID=$(blkid -s UUID -o value "${DEVICE}3")
-
-if [ -z "$UUID" ]; then
-  echo "Error: UUID for $DEVICE not found!"
-  exit 1
-fi
-
-CRYPTTAB_FILE="/etc/crypttab"
-
-# Check if /etc/crypttab exists
-if [ ! -f "$CRYPTTAB_FILE" ]; then
-  echo "Error: $CRYPTTAB_FILE does not exist!"
-  exit 1
-fi
-
-# Add the UUID to /etc/crypttab in the required format
-echo "crypt-home UUID=$UUID none luks" >>"$CRYPTTAB_FILE"
-echo "Successfully added UUID for ${DEVICE}3 to $CRYPTTAB_FILE"
+mount "${DEVICE}3" /mnt/home
 
 echo "Partitions created and mounted successfully."
+
+echo "Enabling system time synchronization"
+timedatectl set-ntp true
+
+echo "Updating mirrorlist"
+reflector -c IN --age 12 --sort rate --protocol https --save /etc/pacman.d/mirrorlist
